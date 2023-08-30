41ad91dbd35de32b7d1f4286a232f14a4465a9d1
roy0206 <swimking_ryu@naver.com>
2023-08-30 15:28:09 +0900

    sgsg

--- Assets/Script/CatController.cs
+++ Assets/Script/CatController.cs
@@ -13,10 +13,7 @@ public class CatController : MonoBehaviour
 
     int moveDir = 1;
 
-    float shotTimeMin = 0.3f;
-    float shotTimeMax = 1f;
-
-    public GameObject bullet;
+    //cat 공격 시 필요한 변수 선언
 
     private void Start()
     {
@@ -45,12 +42,8 @@ public class CatController : MonoBehaviour
 
     IEnumerator shootBullet()
     {
-        float RandTime = Random.RandomRange(shotTimeMin, shotTimeMax);
-        while(true)
-        {
-            Instantiate(bullet,gameObject.transform.position,gameObject.transform.rotation);
-            yield return new WaitForSeconds(RandTime);
-        }
+        //코드 작성 부분 (아래 코드 지워주세요!)
+        yield return null;
         
     }
 }
